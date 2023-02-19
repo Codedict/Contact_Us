6a809ebbf09d0fcb67f8d5a260473e05aff52b4a
Aswin_S <aswinsanthoshnair@gmail.com>
2023-02-19 20:56:08 +0530

    Intial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div>
+      {/* other app components */}
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
